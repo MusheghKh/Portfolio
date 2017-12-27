92d6e8ee0ec27f6580e9821f898167aea0fc1ea5
asddsajpg <asddsajpg@mgail.com>
2017-12-27 15:48:35 +0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AboutComponent } from './about/about.component';
+import { TourComponent } from './tour/tour.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    AboutComponent,
+    TourComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
